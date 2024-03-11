0d866b9c7f09ab7cfcf24b177c22c63a0c6aa092
Aniudupa15 <aniudupa15@gmail.com>
2024-03-11 14:16:57 +0530

    AAK

---
+++ app/src/main/java/com/example/aakrithi2k24/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.aakrithi2k24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+private Button qr;
+private Button dress;
+private FloatingActionButton floatingActionButton2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        qr=findViewById(R.id.qr);
+        floatingActionButton2=findViewById(R.id.floatingActionButton2);
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity (new Intent(MainActivity.this,scanner.class));
+            }
+        });
+        dress=findViewById(R.id.dress);
+        dress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity (new Intent(MainActivity.this,scanner1.class));
+            }
+        });
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
